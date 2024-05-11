08c153f37fe306e93174f422c96bca49949efaa2
last style emo <123621061+Andreygabs@users.noreply.github.com>
2024-05-11 15:19:14 -0400

    Initials

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
+import Sidebar from './Menu/Sidebar'
 
 function App() {
   
 
   return (
     <>
+     <Sidebar/>
      <div> <Outlet/> </div>
     </>
   )
---
+++ src/Menu/SidebarData.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import * as RiIcons from 'react-icons/ri';
+
+ export const SidebarData = [
+  {
+    title: 'Clientes',
+    path: '/Overview',
+    icon: <IoIcons.IoMdHelpCircle  />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+  
+  },
+  {
+    title: 'Produtos',
+    path: '/reports',
+    icon: <IoIcons.IoMdHelpCircle  />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+
+  },
+  {
+    title: 'Promoções',
+    path: '/products',
+    icon: <IoIcons.IoMdHelpCircle  />
+  },
+  {
+    title: 'Ordens de Serviço',
+    path: '/team',
+    icon: <IoIcons.IoMdPeople />
+  },
+  {
+    title: 'Vendas',
+    path: '/messages',
+    icon: <FaIcons.FaEnvelopeOpenText />,
+
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+
+    subNav: [
+      {
+        title: 'Message 1',
+        path: '/messages/message1',
+        icon: <IoIcons.IoIosPaper />
+      },
+      {
+        title: 'Message 2',
+        path: '/messages/message2',
+        icon: <IoIcons.IoIosPaper />
+      }
+    ]
+  },
+  {
+    title: 'Estoque',
+    path: '/support',
+    icon: <IoIcons.IoMdHelpCircle />
+  },
+  {
+    title: 'Financeiro',
+    path: '/support',
+    icon: <IoIcons.IoMdHelpCircle  />
+  },
+  { 
+    title: 'Relatórios',
+    path: '/support',
+    icon: <IoIcons.IoMdHelpCircle  />
+  },
+  {
+    
+    title: 'Configurações',
+    path: '/support',
+    icon: <IoIcons.IoMdHelpCircle  />
+  },
+  {
+    
+    title: 'Chat',
+    path: '/support',
+    icon: <IoIcons.IoMdHelpCircle  />
+  },
+  {
+    
+    title: 'Central de ajuda',
+    path: '/support',
+    icon: <IoIcons.IoMdHelpCircle  />
+  },
+  {
+    
+    title: 'Sair',
+    path: '/support',
+    icon: <IoIcons.IoMdHelpCircle  />
+  }
+];
+
