fd88819e7415a79be0df829eb692f44088a977a6
Ítalo Monte <italopessoal@outlook.com>
2024-05-12 19:39:10 -0700

    dashboard animation

--- src/Componentes/ClienteDashboard/ClienteBotoes.jsx
+++ src/Componentes/ClienteDashboard/ClienteBotoes.jsx
@@ -7,16 +7,14 @@ import './ClienteBotoes.css'
 const ClienteBotoes = () => {
   return (
     <div className='container-dashboard'>
-        <div className="card-opt">
+        <a href='/clienteCadastro/' className="card-opt">
           <h4>Cadastro de clientes</h4>
           <MdPeopleAlt className='icon-people'/>
-          <a href='/clienteCadastro/'><button className='card-bt'>Cadastro de Clientes</button></a>
-        </div>
-        <div className="card-opt">
+        </a>
+        <a href='/clienteBusca/' className="card-opt">
           <h4>Busca de clientes</h4>
           <FaSearch className='icon-lupa'/>
-          <a href='/clienteBusca/'><button className='card-bt'>Busca de Clientes</button></a>
-        </div>
+        </a>
     </div>
   )
 }
