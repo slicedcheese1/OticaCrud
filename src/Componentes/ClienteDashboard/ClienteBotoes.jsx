efb0991e2856a045ff4e8b14ad623416c986c02b
Ítalo Monte <italopessoal@outlook.com>
2024-05-12 19:29:11 -0700

    links corretos

--- src/Componentes/ClienteDashboard/ClienteBotoes.jsx
+++ src/Componentes/ClienteDashboard/ClienteBotoes.jsx
@@ -10,12 +10,12 @@ const ClienteBotoes = () => {
         <div className="card-opt">
           <h4>Cadastro de clientes</h4>
           <MdPeopleAlt className='icon-people'/>
-          <a href='http://localhost:5174/clienteCadastro/'><button className='card-bt'>Cadastro de Clientes</button></a>
+          <a href='/clienteCadastro/'><button className='card-bt'>Cadastro de Clientes</button></a>
         </div>
         <div className="card-opt">
           <h4>Busca de clientes</h4>
           <FaSearch className='icon-lupa'/>
-          <a href='http://localhost:5173/clienteBusca/'><button className='card-bt'>Busca de Clientes</button></a>
+          <a href='/clienteBusca/'><button className='card-bt'>Busca de Clientes</button></a>
         </div>
     </div>
   )
