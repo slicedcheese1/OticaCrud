bf0ea81d2e6b657033fc04345602c7fe4d12e2e3
last style emo <123621061+Andreygabs@users.noreply.github.com>
2024-05-14 21:13:08 -0400

    ASAS
    asdasd

---
+++ src/Sistema.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from 'react-router-dom'
+import Sidebar from './Menu/Sidebar'
+import { useState } from 'react'
+
+function Sistema() {
+  // define estado da sidebar
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => {setSidebar(!sidebar);};
+
+  // ln 15 - apenas 'container' se 'sidebar' estiver fechado, caso contrario, classe tambem chamada de 'sidebar-open'(para o css)
+  return (
+    <>
+      <Sidebar sidebar={sidebar} showSidebar={showSidebar} />
+      <div className={`container ${sidebar ? 'sidebar-open' : ''}`}>
+        <Outlet />
+      </div>
+    </>
+  )
+}
+
+export default Sistema;
+ 
