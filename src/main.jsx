24d903af4e8eb775d69ede0c7746c6a3c82c4090
Vhvo-2004 <vhvdo.eng22@uea.edu.br>
2024-05-17 23:02:29 -0400

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,58 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import Clientes from './Pages/ClienteCadastro.jsx'
-import ClienteBusca from './Pages/ClienteBusca.jsx'
-import ClienteCadastro from './Pages/ClienteCadastro.jsx'
-import ClienteDashboard from './Pages/Cliente.jsx'
-import Login from './Componentes/UsuárioCadastro/Login.jsx'
-import Sistema from './Sistema.jsx'
-
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+//import Clientes from './Pages/ClienteCadastro.jsx';
+import ClienteBusca from './Pages/ClienteBusca.jsx';
+import ClienteCadastro from './Pages/ClienteCadastro.jsx';
+import ClienteDashboard from './Pages/Cliente.jsx';
+import Login from './Componentes/UsuárioCadastro/Login.jsx';
+import Sistema from './Sistema.jsx';
+import './index.css';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App/>,
-
+    path: '/',
+    element: <App />,
     children: [
       {
-       path: "/Sistema",
-       element: <Sistema/>,
-       children:[
-        {
-          path: "/Sistema/Clientes",
-          element: <ClienteDashboard/>
-        },
-        {
-          path: "/Sistema/ClienteCadastro",
-          element: <ClienteCadastro/>
-        },
-        {
-          path: "/Sistema/ClienteBusca",
-          element: <ClienteBusca/>
-        },
-        
-        
-       ]
+        path: '/Sistema',
+        element: <Sistema />,
+        children: [
+          {
+            path: '/Sistema/Clientes',
+            element: <ClienteDashboard />
+          },
+          {
+            path: '/Sistema/ClienteCadastro',
+            element: <ClienteCadastro />
+          },
+          {
+            path: '/Sistema/ClienteBusca',
+            element: <ClienteBusca />
+          }
+        ]
       },
-      
       {
-        path: "/Login",
-        element: <Login/>
+        path: '/Login',
+        element: <Login />
       },
       
     ]
-  },
-
-])
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router = {router} />
-  </React.StrictMode>,
-)
-
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
