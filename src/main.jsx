7af5507b89dc1bcc12b242a648026cff63c72b37
Vhvo-2004 <vhvdo.eng22@uea.edu.br>
2024-05-17 23:40:26 -0400

    Login adicionado!

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 //import Clientes from './Pages/ClienteCadastro.jsx';
-import ClienteBusca from './Pages/ClienteBusca.jsx';
-import ClienteCadastro from './Pages/ClienteCadastro.jsx';
-import ClienteDashboard from './Pages/Cliente.jsx';
-import Login from './Componentes/UsuárioCadastro/Login.jsx';
+import ClienteBusca from './Pages/Clientes/ClienteBusca.jsx';
+import ClienteCadastro from './Pages/Clientes/ClienteCadastro.jsx';
+import ClienteDashboard from './Pages/Clientes/ClienteDashboard.jsx';
+import Login from './Pages/Login/Login.jsx';
 import Sistema from './Sistema.jsx';
 import './index.css';
 
