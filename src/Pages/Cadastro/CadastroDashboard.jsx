e041dd2571885a15accb4444effe2e84bf74dc18
Vhvo-2004 <vhvdo.eng22@uea.edu.br>
2024-06-17 02:32:37 -0400

    A página com os botoes dentro de um card
    Págian com card agora

--- src/Pages/Cadastro/CadastroDashboard.jsx
+++ src/Pages/Cadastro/CadastroDashboard.jsx
@@ -4,9 +4,14 @@ import CadastroBotoes2 from '../../Componentes/Cadastro/CadastroDashboard/Cadast
 const CadastroDashboard = () => {
   return (
       <>
-        <h2>Cadastros</h2>
-        <hr/>
-        <CadastroBotoes2/>
+        <div className="page-container">
+          <div className="card-main">
+            <h2>Cadastros</h2>
+            <hr/>
+        
+            <CadastroBotoes2/>
+          </div>
+        </div>
       </>
   )
 }
