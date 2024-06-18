3c2dde3520ea8afe0864ad2aa734d9f012fa0de0
Ítalo Monte <italopessoal@outlook.com>
2024-06-18 19:26:40 -0400

    estruturando cadastro

---
+++ src/Pages/Funcionarios/CadastroFuncionario.jsx
@@ -0,0 +1,13 @@
+import { React, useState } from "react"
+
+function CadastroFuncionario() {
+
+    const [] = useState()
+
+    return(<>
+    
+    </>
+    )
+}
+
+export default CadastroFuncionario
