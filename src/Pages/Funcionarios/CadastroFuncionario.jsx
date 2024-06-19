2bd44436948577d38657e57358b2000c4dd9c2db
Ítalo Monte <italopessoal@outlook.com>
2024-06-18 21:02:12 -0400

    states funcionario

--- src/Pages/Funcionarios/CadastroFuncionario.jsx
+++ src/Pages/Funcionarios/CadastroFuncionario.jsx
@@ -2,7 +2,45 @@ import { React, useState } from "react"
 
 function CadastroFuncionario() {
 
-    const [] = useState()
+    // {
+    //     "id": 0,
+    //     "nome": "string",
+    //     "senha": "string",
+    //     "lojaCadastro": "string",
+    //     "cargos": [
+    //       {
+    //         "idCargo": 0,
+    //         "nomeCargo": "string"
+    //       }
+    //     ],
+    //     "rg": "string",
+    //     "cpf": "string",
+    //     "telefonePrimario": "string",
+    //     "telefoneSecundario": "string",
+    //     "cep": "string",
+    //     "endereco": "string",
+    //     "bairro": "string",
+    //     "cidade": "string",
+    //     "limiteDesconto": 0,
+    //     "email": "string",
+    //     "loja": "string"
+    //   }
+
+    const [nome, setNome] = useState("")
+    const [senha, setSenha] = useState("")
+    const [lojaCadastro, setLojaCadastro] = useState("")
+    const [set, setCargos] = useState([])
+    const [rg, setRg] = useState("")
+    const [cpf, setCpf] = useState("")
+    const [telefonePrimario, setTelefonePrimario] = useState("")
+    const [telefoneSecundario, setTelefoneSecundario] = useState("")
+    const [cep, setCep] = useState("")
+    const [endereco, setEndereco] = useState("")
+    const [bairro, setBairro] = useState("")
+    const [limiteDesconto, setLimiteDesconto] = useState("")
+    const [email, setEmail] = useState("")
+    const [loja, setLoja] = useState("")
+    const [loja1, setLoja1] = useState("")
 
     return(<>
     
