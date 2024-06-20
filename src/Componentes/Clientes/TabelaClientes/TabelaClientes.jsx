d127338badd55a5450a1182890bf876ae8b4eb24
last style emo <123621061+Andreygabs@users.noreply.github.com>
2024-06-20 05:05:42 -0400

    as

--- src/Componentes/Clientes/TabelaClientes/TabelaClientes.jsx
+++ src/Componentes/Clientes/TabelaClientes/TabelaClientes.jsx
@@ -42,7 +42,10 @@ const ClienteBusca = () => {
         <hr />
         <br />
         <label>Buscar um cliente</label>
-        <input placeholder="Procure por Nome, CPF, CNPJ"type= "text" onChange={(ev)=>setSearch(ev.target.value)} value={search}/>
+        <input placeholder="Procure por Nome, CPF, CNPJ" 
+        type= "text" 
+        onChange={(ev)=>setSearch(ev.target.value)} 
+        value={search}/>
         <br />
         <br />
       </div>
@@ -62,7 +65,7 @@ const ClienteBusca = () => {
           </thead>
           <tbody>
             {clientes.filter(cliente =>
-              search.trim() === '' ? cliente : cliente.nome.toLowerCase().includes()
+              search.trim() === '' ? cliente : cliente.nome.toLowerCase().includes(lowerSearch)
             ).map((cliente) => (
               <tr key={cliente.idCliente}>
                 <td>{cliente.nome}</td>
