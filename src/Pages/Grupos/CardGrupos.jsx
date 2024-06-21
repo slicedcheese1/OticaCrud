97a1945d8125ceba20778206c9972e5587962976
Italo Monte <italopessoal@outlook.com>
2024-06-21 18:41:49 -0400

    usando module.css nos grupos

--- src/Pages/Grupos/CardGrupos.jsx
+++ src/Pages/Grupos/CardGrupos.jsx
@@ -2,38 +2,35 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Dropdown from 'react-bootstrap/Dropdown';
 
-import './CardGrupos.css';
+import styles from './CardGrupos.module.css';
 function CardGrupos() {
   return (
     <Card>
       <Card.Header id="Titulo-Card">Novo Grupo de Produtos</Card.Header>
-      <hr></hr>
+      <hr/>
       <Card.Body>
         <blockquote className="blockquotemb-0">
         <Form>
-        
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label id="Nome-Produto">Novo Produto</Form.Label>
-        
-        <div className="d-flex">
-        <Form.Control  type="Produto" placeholder="Descrição do Grupo" />
-        
-        <div className="c-flex">
-        
-        <h4 id="Text-cat">Categoria</h4>
-        <Form.Select aria-label="Default select example">
-        
-      <option>Produto</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </Form.Select>
-      </div>
-      
-      </div>
-      </Form.Group>
-        </Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label id="NomeProduto">Novo Produto</Form.Label>
           
+          <div className={styles.dFlex}>
+            <Form.Control  type="Produto" placeholder="Descrição do Grupo" />
+            
+            <div className={styles.cFlex}>
+            
+            <h4 id="Text-cat">Categoria</h4>
+            <Form.Select aria-label="Default select example">
+              <option>Produto</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+            </div>
+        
+          </div>
+          </Form.Group>
+        </Form>
         </blockquote>
       </Card.Body>
     </Card>
