2a60c4a094f62d45db2566ce9e92fdeef738de29
Italo Monte <italopessoal@outlook.com>
2024-06-23 18:13:40 -0400

    Post funcionando

--- src/Pages/Lojas/CadastroLojas.jsx
+++ src/Pages/Lojas/CadastroLojas.jsx
@@ -1,81 +1,113 @@
-    //     "nomeFantasia": "string",
-    //     "razaoSocial": "string",
-    //     "laboratorio": true,
-    //     "cep": "string",
-    //     "endereco": "string",
-    //     "numero": "string",
-    //     "complemento": "string",
-    //     "bairro": "string",
-    //     "email": "string",
-    //     "website": "string",
-    //     "inscricaoEstadual": "string",
-    //     "inscricaoMunicipal": "string",
-    //     "suframa": "string",
-    //     "contribuinteICMS": true,
-    //     "observacoes": "string",
-    //     "telefone": "string"
-    //   }
+import React from 'react'
+import { useState } from 'react'
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
 
-    const [cpf, setCpf] = useState("")
-    const [cnpj, setCnpj] = useState("")
-    const [nomeFantasia, setNomeFantasia] = useState("")
-    const [razaoSocial, setRazaoSocial] = useState("")
-    const [identificacaoEmpresa, setIdentificacaoEmpresa] = useState("")
-    const [telefonePrincipal, setTelefonePrincipal] = useState("")
-    const [telefoneSecundario, setTelefoneSecundario] = useState("")
-    const [cep, setCep] = useState("")
-    const [numero, setNumero] = useState("")
-    const [complemento, setComplemento] = useState("")
-    const [bairro, setBairro] = useState("")
-    const [cidade, setCidade] = useState("")
-    
+const CadastroLojas = () => {
 
-    function saveLoja(e){
-        e.preventDefault()
+  // {
+  //   "idLoja": 0,
+  //   "cpf": "string",
+  //   "cnpj": "string",
+  //   "razaoSocial": "string",
+  //   "nomeFantasia": "string",
+  //   "identificacaoEmpresa": "string",
+  //   "telefonePrincipal": "+55916257324103328",
+  //   "telefoneSecundario": "+6587709284999883",
+  //   "nomeContato": "string",
+  //   "email": "string",
+  //   "cep": "98015-304",
+  //   "numero": 0,
+  //   "bairro": "string",
+  //   "complemento": "string",
+  //   "cidade": "string"
+  // }
 
-        const lojas = JSON.stringify({
-          cpf,
-          cnpj,
-          razaoSocial,
-          nomeFantasia,
-          identificacaoEmpresa,
-          telefonePrincipal,
-          telefoneSecundario,
-          cep,
-          numero,
-          bairro,
-          complemento,
-          cidade
+  const [cpf, setCpf] = useState("")
+  const [cnpj, setCnpj] = useState("")
+  const [razaoSocial, setRazaoSocial] = useState("")
+  const [nomeFantasia, setNomeFantasia] = useState("")
+  const [identificacaoEmpresa, setIdentificacaoEmpresa] = useState("")
+  const [telefonePrincipal, setTelefonePrincipal] = useState("")
+  const [telefoneSecundario, setTelefoneSecundario] = useState("")
+  const [nomeContato, setNomeContato] = useState("")
+  const [email, setEmail] = useState("")
+  const [cep, setCep] = useState("")
+  const [numero, setNumero] = useState("")
+  const [bairro, setBairro] = useState("")
+  const [complemento, setComplemento] = useState("")
+  const [cidade, setCidade] = useState("")
+  
+
+  function saveLoja(e){
+      e.preventDefault()
+
+      const loja = JSON.stringify({
+        cpf,
+        cnpj,
+        razaoSocial,
+        nomeFantasia,
+        identificacaoEmpresa,
+        telefonePrincipal,
+        telefoneSecundario,
+        nomeContato,
+        email,
+        cep,
+        numero,
+        bairro,
+        complemento,
+        cidade
+      })
+
+      console.log(loja)
+
+      fetch('http://localhost:8080/loja', {
+          //mode: 'no-cors',
+          method: 'POST',
+          body: loja,
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
         })
-
-        console.log(lojas)
-
-        fetch('http://localhost:8080/loja', {
-            //mode: 'no-cors',
-            method: 'POST',
-            body: lojas,
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            }
+          .then(response => response.json())
+          .then((response) => {
+            console.log('Fornecedor criado com sucesso:', response);
           })
-            .then(response => response.json())
-            .then((response) => {
-              console.log('Fornecedor criado com sucesso:', response);
-            })
-            .catch((error) => {
-              console.error('Erro ao criar post:', error);
-            });
-    }
+          .catch((message) => {
+            console.error('Erro ao criar post:', message, message.message);
+          });
+  }
 
-    return(
-      <>
-        <form>
-            <div className='container-dados-pessoais-funcionario'>
-                <h2>Nova Loja</h2>
-                <hr/>
-                
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+  return(
+    <>
+      <form>
+          <div>
+              <h2>Nova Loja</h2>
+              <hr/>
+              <Form>
+
+                <Form.Group>
+                  <Form.Label>Nome de contato</Form.Label>
+                  <Form.Control 
+                    type="text" 
+                    placeholder="Insita o nome da loja" 
+                    value={nomeContato}
+                    onChange={(e) => {setNomeContato(e.target.value)}}
+                  />
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label>email de contato</Form.Label>
+                  <Form.Control 
+                    type="text" 
+                    placeholder="Insita o email da loja" 
+                    value={email}
+                    onChange={(e) => {setEmail(e.target.value)}}
+                  />
+                </Form.Group>
+
+                <Form.Group>
                   <Form.Label>CPF</Form.Label>
                   <Form.Control 
                     type="text" 
@@ -97,15 +129,15 @@
                 </Form.Group>
 
                 <Form.Group>
-                        <Form.Label>Identifcação da loja no sistema</Form.Label>
-                        <Form.Control
-                            name="identificacaoEmpresa"
-                            type="text"
-                            placeholder="identificacaoEmpresa"
-                            value={identificacaoEmpresa}
-                            onChange={(e) => setIdentificacaoEmpresa(e.target.value)}
-                        />
-                    </Form.Group>
+                  <Form.Label>Identifcação da loja no sistema</Form.Label>
+                  <Form.Control
+                      name="identificacaoEmpresa"
+                      type="text"
+                      placeholder="identificacaoEmpresa"
+                      value={identificacaoEmpresa}
+                      onChange={(e) => setIdentificacaoEmpresa(e.target.value)}
+                  />
+                </Form.Group>
                     
 
                 <Form.Group>
@@ -124,82 +156,83 @@
                   <Form.Control 
                     name="razaoSocial"
                     type='text' 
-                   
+                    
                     value={razaoSocial}
                     onChange={(e) => {setRazaoSocial(e.target.value)}}
                   />
                 </Form.Group>
 
                 <Form.Group>
-                        <Form.Label>Telefone Principal</Form.Label>
-                        <Form.Control
-                            name="bairro"
-                            type="text"
-                            placeholder=""
-                            value={telefonePrincipal}
-                            onChange={(e) => setTelefonePrincipal(e.target.value)}
-                        />
-                    </Form.Group>
-
-                    <Form.Group>
-                        <Form.Label>Telefone Secundário</Form.Label>
-                        <Form.Control
-                            name="bairro"
-                            type="text"
-                            placeholder="Bairro"
-                            value={telefoneSecundario}
-                            onChange={(e) => setTelefoneSecundario(e.target.value)}
-                        />
-                    </Form.Group>
-                    
-                    
-
-                   <Form.Group>
-                    <Form.Label>CEP</Form.Label>
-                    <Form.Control
-                        name="cep"
-                        type="text"
-                        placeholder="CEP"
-                        value={cep}
-                        onChange={(e) => setCep(e.target.value)}
-                    />
+                  <Form.Label>Telefone Principal</Form.Label>
+                  <Form.Control
+                      name="bairro"
+                      type="text"
+                      placeholder=""
+                      value={telefonePrincipal}
+                      onChange={(e) => setTelefonePrincipal(e.target.value)}
+                  />
                 </Form.Group>
 
-                   
+                <Form.Group>
+                    <Form.Label>Telefone Secundário</Form.Label>
+                    <Form.Control
+                        name="bairro"
+                        type="text"
+                        placeholder="Bairro"
+                        value={telefoneSecundario}
+                        onChange={(e) => setTelefoneSecundario(e.target.value)}
+                    />
+                </Form.Group>
+                    
+                    
 
-                    <Form.Group>
-                        <Form.Label>Número</Form.Label>
-                        <Form.Control
-                            name="numero"
-                            type="text"
-                            placeholder="Número"
-                            value={numero}
-                            onChange={(e) => setNumero(e.target.value)}
-                        />
-                    </Form.Group>
+                <Form.Group>
+                  <Form.Label>CEP</Form.Label>
+                  <Form.Control
+                      name="cep"
+                      type="text"
+                      placeholder="CEP"
+                      value={cep}
+                      onChange={(e) => setCep(e.target.value)}
+                  />
+                </Form.Group>
 
-                    <Form.Group>
-                        <Form.Label>Complemento</Form.Label>
-                        <Form.Control
-                            name="complemento"
-                            type="text"
-                            placeholder="Complemento"
-                            value={complemento}
-                            onChange={(e) => setComplemento(e.target.value)}
-                        />
-                    </Form.Group>
+                    
 
-                    <Form.Group>
-                        <Form.Label>Bairro</Form.Label>
-                        <Form.Control
-                            name="bairro"
-                            type="text"
-                            placeholder="Bairro"
-                            value={bairro}
-                            onChange={(e) => setBairro(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Form.Group>
+                <Form.Group>
+                  <Form.Label>Número</Form.Label>
+                  <Form.Control
+                      name="numero"
+                      type="text"
+                      placeholder="Número"
+                      value={numero}
+                      onChange={(e) => setNumero(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label>Complemento</Form.Label>
+                  <Form.Control
+                      name="complemento"
+                      type="text"
+                      placeholder="Complemento"
+                      value={complemento}
+                      onChange={(e) => setComplemento(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label>Bairro</Form.Label>
+                  <Form.Control
+                      name="bairro"
+                      type="text"
+                      placeholder="Bairro"
+                      value={bairro}
+                      onChange={(e) => setBairro(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group>
                   <Form.Label>Cidade</Form.Label>
                   <Form.Control
                     name="Cidade"
@@ -208,31 +241,15 @@
                     onChange={(e) => {setCidade(e.target.value)}}
                   />
                 </Form.Group>
+              </Form>
+          </div>
+          <Button onClick={(e) => saveLoja(e)}>Cadastrar</Button>
 
-                    
-                    
-
-            
-
-                 
-
-                
-
-                   
-
-                 
-                 
-
-            </div>
-            <button
-            onClick={(e) => saveLoja(e)}>Cadastrar</button>
-
-            <div className='container-dados-usuario'>
-            
-            </div>
-        </form>
-      </>
-    )
+          <div className='container-dados-usuario'>
+          </div>
+      </form>
+    </>
+  )
 }
 
 export default CadastroLojas
